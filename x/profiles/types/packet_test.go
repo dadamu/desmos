5eec0ace2fecb960fec4dec96ea9a1f4cb704377
Paul <p22626262@gmail.com>
2021-06-01 15:46:32 +0800

    Add packet tests

---
+++ x/profiles/types/packet_test.go
@@ -0,0 +1,48 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/stretchr/testify/require"
+
+	"github.com/desmos-labs/desmos/x/profiles/types"
+)
+
+func TestLinkChainAccountPacketData_Validate(t *testing.T) {
+	tests := []struct {
+		name   string
+		packet types.LinkChainAccountPacketData
+		expErr error
+	}{
+		{
+			name: "Valid packet returns no error",
+			packet: types.NewLinkChainAccountPacketData(
+				"cosmos1yt7rqhj0hjw92ed0948r2pqwtp9smukurqcs70",
+				types.NewProof(secp256k1.GenPrivKey().PubKey(), "032086ede8d4bce29fe364a94744ca71dbeaf370221ba20f9716a165c54b079561", "plain_text"),
+				types.NewChainConfig("cosmos", "cosmos"),
+				"cosmos1yt7rqhj0hjw92ed0948r2pqwtp9smukurqcs70",
+				types.NewProof(secp256k1.GenPrivKey().PubKey(), "032086ede8d4bce29fe364a94744ca71dbeaf370221ba20f9716a165c54b079561", "plain_text"),
+			),
+			expErr: nil,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			require.Equal(t, test.expErr, test.packet.Validate())
+		})
+	}
+}
+
+func TestLinkChainAccountPacketData_GetBytes(t *testing.T) {
+	p := types.NewLinkChainAccountPacketData(
+		"cosmos1yt7rqhj0hjw92ed0948r2pqwtp9smukurqcs70",
+		types.NewProof(secp256k1.GenPrivKey().PubKey(), "032086ede8d4bce29fe364a94744ca71dbeaf370221ba20f9716a165c54b079561", "plain_text"),
+		types.NewChainConfig("cosmos", "cosmos"),
+		"cosmos1yt7rqhj0hjw92ed0948r2pqwtp9smukurqcs70",
+		types.NewProof(secp256k1.GenPrivKey().PubKey(), "032086ede8d4bce29fe364a94744ca71dbeaf370221ba20f9716a165c54b079561", "plain_text"),
+	)
+	_, err := p.GetBytes()
+	require.NoError(t, err)
+}
